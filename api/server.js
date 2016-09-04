82c59091379499e2d933196405e9aea60373b27e
jaywonchun <jcsince93@gmail.com>
2016-09-04 15:43:13 -0400

    finalfinal

--- api/server.js
+++ api/server.js
@@ -67,8 +67,8 @@ pal.on('connection', function(socket){
 
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
 
 /*SOCKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET*/
